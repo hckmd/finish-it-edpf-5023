70b4b38b6637f691308671268cba892f43e39ea3
hckmd <daniel.hickmott@gmail.com>
2021-06-19 17:09:03 +1000

    Adds templates for potential milestone 1 example

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template, request
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Finish it - Home'
+    return render_template('index.html', title = 'Home')
+
+@app.get('/books')
+def books_list():
+    return render_template('books_list.html', title = 'Books')
+
+@app.route('/add_book', methods = ['GET','POST'])
+def add_book():
+    if request.method == 'GET':
+        return render_template('add_book.html', title = 'Add book')
+    else:
+        return render_template('book_success.html', title = 'Added Book Successfully')
