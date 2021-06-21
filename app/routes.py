272782d5e9c5d0fa41cad6957ab9dc11a74b0f2a
hckmd <daniel.hickmott@gmail.com>
2021-06-21 15:51:14 +1000

    Minor changes to imports on main routes

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,6 @@
-from os import stat
-from flask import render_template, request, redirect, url_for
+from flask import render_template
 
 from app import app
-from app import status_options, priority_options
-from app.models import Book, Tag
 
 @app.route('/')
 @app.route('/index')
