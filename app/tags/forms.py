a13da7d895124fbb59436337ec40317f6cac9b0e
hckmd <daniel.hickmott@gmail.com>
2021-06-21 19:47:05 +1000

    Changes tag list to allow editing tags in the table

--- app/tags/forms.py
+++ app/tags/forms.py
@@ -1,8 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms.widgets import HiddenInput
 
 from app.models import Tag
+
+class TagEditForm(FlaskForm):
+    '''Form for editing an existing tag'''
+    id = IntegerField(widget=HiddenInput())
+    name = StringField('Tag name', validators=[DataRequired(), Length(max=100)])
+    submit = SubmitField('Save changes')
+
+    def validate_name(form, field):
+        if field.raw_data:
+            # Make all tag names lowercase for simplicity of keeping them unique
+            field.data = field.data.lower()
+
+            # Check if tag already exists and whether it's not the one being saved
+            existing_tag = Tag.query.filter_by(name=field.data).first()
+            if existing_tag:
+                # If the tag already exists, but it's not this one
+                # get the user to save as a different name to avoid duplication
+                if existing_tag.id != id:
+                    raise ValidationError('Tag name must be unique')
+        else:
+            raise ValidationError('Tag name cannot be blank')
 
 class TagAddForm(FlaskForm):
     '''Form for creating a new tag'''
@@ -13,8 +35,10 @@ class TagAddForm(FlaskForm):
         if field.raw_data:
             # Make all tag names lowercase for simplicity of keeping them unique
             field.data = field.data.lower()
-            tag_name = Tag.query.filter_by(name=field.data).first()
-            if tag_name:
+
+            # Check if the tag already exists - if so, it shouldn't be added
+            existing_tag = Tag.query.filter_by(name=field.data).first()
+            if existing_tag:
                 raise ValidationError('Tag name must be unique')
         else:
             raise ValidationError('Tag name cannot be blank')
--- app/tags/routes.py
+++ app/tags/routes.py
@@ -1,24 +1,40 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 
 from app import db
 from app.models import Tag
 from app.tags import bp
-from .forms import TagAddForm
+from .forms import TagAddForm, TagEditForm
 
 @bp.route('/', methods = ['GET', 'POST'])
 @bp.route('/index', methods = ['GET', 'POST'])
 def index():
+    editing = False
+    edit_tag = None
+    editing_tag_id = 0
+    add_form = TagAddForm()
+    edit_form = TagEditForm()
+
+    if 'tag_id' in request.args:
+        editing_tag_id = int(request.args['tag_id'])
+        edit_tag = Tag.query.get_or_404(editing_tag_id)
+        editing = True
+        edit_form = TagEditForm(obj=edit_tag)
     tags = Tag.query.all()
-    form = TagAddForm()
-    if form.validate_on_submit():
+
+    if add_form.validate_on_submit():
+        print('Running this add_form')
         tag = Tag()
-        form.populate_obj(tag)
+        add_form.populate_obj(tag)
         db.session.add(tag)
         db.session.commit()
 
         return redirect(url_for('tags.index'))
 
-    return render_template('tags_list.html', title='Tags', tags=tags, form=form)
+    return render_template('tags_list.html', 
+        title='Tags', tags=tags, 
+        editing = editing, editing_tag_id=editing_tag_id,
+        add_form=add_form, edit_form=edit_form
+    )
 
 @bp.route('/delete/<int:id>')
 def delete(id):
@@ -27,3 +43,21 @@ def delete(id):
     db.session.delete(to_delete)
     db.session.commit()
     return render_template('tag_delete_success.html', title='Tag Deleted', tag_name=tag_name)
+
+@bp.post('/save/<int:id>')
+def save(id):
+    edit_form = TagEditForm()
+    add_form = TagAddForm()
+    tags = Tag.query.all()
+    if edit_form.validate_on_submit():
+        tag = Tag.query.get_or_404(id)
+        edit_form.populate_obj(tag)
+        db.session.add(tag)
+        db.session.commit()
+        return redirect(url_for('tags.index'))
+
+    return render_template('tags_list.html', 
+        title='Tags', tags=tags, 
+        editing = True, editing_tag_id=id,
+        add_form=add_form, edit_form=edit_form
+    )
