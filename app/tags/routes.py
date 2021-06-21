e3985fe2ebd7cfde01e666a85c696accfef28f52
hckmd <daniel.hickmott@gmail.com>
2021-06-21 15:44:27 +1000

    Adds deletion functionality for Tags blueprint

--- app/tags/routes.py
+++ app/tags/routes.py
@@ -9,3 +9,11 @@ from app.tags import bp
 def index():
     tags = Tag.query.all()
     return render_template('tags_list.html', title='Tags', tags=tags)
+
+@bp.route('/delete/<int:id>')
+def delete(id):
+    to_delete = Tag.query.get_or_404(id)
+    tag_name = to_delete.name
+    db.session.delete(to_delete)
+    db.session.commit()
+    return render_template('tag_delete_success.html', title='Tag Deleted', tag_name=tag_name)
