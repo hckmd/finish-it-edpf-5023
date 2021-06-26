e815e8acf56aef7a151034501012827f3c2ae93b
hckmd <daniel.hickmott@gmail.com>
2021-06-26 10:37:36 +1000

    Adds edit user details form and route

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from wtforms.widgets import HiddenInput
 
 from app.models import User
 
@@ -24,4 +25,22 @@ class UserAddForm(FlaskForm):
         if user != None:
             raise ValidationError('Email address must be unique.') 
         
+class UserEditDetailsForm(FlaskForm):
+    id = IntegerField(widget=HiddenInput())
+    username = StringField('Username', validators=[DataRequired(), Length(max=64)])
+    email = StringField('Email', validators=[DataRequired(), Length(max=120)])
+    is_administrator = BooleanField('Is administrator?')
+    submit = SubmitField('Save changes')
 
+    def validate_username(form, field):
+        # Check if the username already exists and doesn't belong to this user
+        user = User.query.filter_by(username = field.data).first()
+        if user != None and user.id != form.id.data:
+            raise ValidationError('User name must be unique.')
+
+    def validate_email(form, field):
+        # Check if a user with the email already exists and it's not this one
+        user = User.query.filter_by(email = field.data).first()
+        if user != None and user.id != form.id.data:
+            raise ValidationError('Email address must be unique.') 
+    
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -5,7 +5,7 @@ from app import db
 from app.models import User
 from app.admin import bp
 from .decorators import admin_required
-from .forms import UserAddForm
+from .forms import UserAddForm, UserEditDetailsForm
 
 @bp.get('/')
 @bp.get('/index')
@@ -35,6 +35,33 @@ def add_user():
         return redirect(url_for('admin.index'))
 
     return render_template('add_user.html', title = 'Add User', form = form)
+
+
+@bp.route('/edit_user/<int:id>', methods = ['GET', 'POST'])
+@login_required
+@admin_required
+def edit_user(id):
+    user = User.query.get_or_404(id)
+    form = UserEditDetailsForm(obj=user)
+    page_title = f'Editing {user.username} details'
+    if form.validate_on_submit():
+
+        # This view only allows change of name and email address
+        # Changing password is a separate view
+        user.username = form.username.data
+        user.email = form.email.data
+        user.is_administrator = form.is_administrator.data
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('admin.index'))
+
+    return render_template(
+        'edit_user.html', 
+        title = page_title, 
+        form = form
+    )
+
 
 @bp.route('/delete/<int:id>')
 @login_required
