f2bd48b4da15194e529d6644d2d4b6b0edc6baa1
hckmd <daniel.hickmott@gmail.com>
2021-06-27 21:26:00 +1000

    Changes base layout to use icons for flashed messages

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -14,7 +14,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid username or password', 'error')
             return redirect(url_for('auth.login'))
         login_user(user)
         return redirect(url_for('index'))
