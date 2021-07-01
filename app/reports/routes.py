4f171195cded8148d2ba3a3eccfae20899de9e0b
hckmd <daniel.hickmott@gmail.com>
2021-07-01 20:07:19 +1000

    Adds users_by_type report for administrator, refactoring of items report to include more status options

--- app/reports/routes.py
+++ app/reports/routes.py
@@ -6,9 +6,10 @@ import plotly
 import plotly.express as px
 from sqlalchemy import func
 
-from app import db
+from app import db, STATUS_OPTIONS
 from app.reports import bp
-from app.models import Item, Tag
+from app.models import Item, User
+from app.admin.decorators import admin_required
 
 @bp.route('/')
 @bp.route('/index')
@@ -16,7 +17,7 @@ from app.models import Item, Tag
 def index():
     return render_template('reports.html', title = 'Reports')
 
-@bp.route('/item_status')
+@bp.get('/item_status')
 @login_required
 def item_status_view():
     return render_template('items_by_status.html', title = 'Items by Status')
@@ -31,6 +32,16 @@ def item_status_data():
     # Convert the result to a list of dictionaries, for use for plotly plot's data source
     item_status_data = [dict(result) for result in item_status_data]
 
+    # Create entries in the data for status options and types that dont result from the data
+    item_types = ['book','course']
+    for status in STATUS_OPTIONS:
+        for item_type in item_types:
+            existing_data = [result for result in item_status_data 
+                if result['status'] == status and result['type'] == item_type]
+            if len(existing_data) == 0:
+                missing_data = {'status': status, 'type': item_type, 'count': 0}
+                item_status_data.append(missing_data)
+
     # Creates a figure with plotly express using the item status data
     figure = px.bar(item_status_data, 
         x = 'status', y = 'count', color = 'type', barmode = 'group',
@@ -39,3 +50,37 @@ def item_status_data():
     # Converts data from plotly express to JSON for the client side
     graphJSON = json.dumps(figure, cls = plotly.utils.PlotlyJSONEncoder)
     return graphJSON
+
+@bp.get('/user_type_data')
+@admin_required
+@login_required
+def user_type_data():
+    # Queries the Items table to get the number of users that are admin and non-admin
+    user_type_data = db.session.query(
+        User.is_administrator.label('type'), func.count(User.id).label('count')
+        ).group_by(User.is_administrator).all()
+    # Convert the result to a list of dictionaries, for use for plotly plot's data source
+    user_type_data = [dict(result) for result in user_type_data]
+
+    # Change the values of the type from False or True to more clear value
+    for row in user_type_data:
+        if row['type'] == False:
+            row['type'] = 'Normal user'
+        else:
+            row['type'] = 'Admin'
+
+    # Creates a figure with plotly express using the item status data
+    figure = px.bar(user_type_data, 
+        x = 'type', y = 'count',
+        labels = {'type': 'User type', 'count': 'Number of users'}
+    )
+    # Converts data from plotly express to JSON for the client side
+    graphJSON = json.dumps(figure, cls = plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+@bp.get('/user_type')
+@admin_required
+@login_required
+def user_type_view():
+    return render_template('users_by_type.html', title = 'Users by Type')
+
